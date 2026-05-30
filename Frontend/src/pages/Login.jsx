efe31095697401dc305053c258471f55fc04d7a1
Shreyanshu Gupta <himanshuguptakir@gmail.com>
2026-05-30 13:45:09 +0530

    initialize React frontend project with Vite, Tailwind CSS, and page routing

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function login() {
+  return(
+    <div className='bg-black text-white w-full h-screen flex justify-center items-center'>
+      <h1 className='text-4xl font-bold underline'>LOGIN</h1>
+    </div>
+  )
+}
+
+export default login
---
+++ Frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function signup() {
+  return(
+    <div>
+      <h1>SIGNUP</h1>
+    </div>
+  )
+}
+
+export default signup
